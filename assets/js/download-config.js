f24b383fcdc0167d4e0e56bac4713e5607b53c8b
LordIco <ico@terra.com.br>
2026-09-03 16:22:27 -0300

    Adiciona link remoto de compra

--- assets/js/download-config.js
+++ assets/js/download-config.js
@@ -1,5 +1,6 @@
 window.PRINTLY_DOWNLOAD_CONFIG = {
   SUPABASE_URL: 'https://uiesczevcmkzdtybxuez.supabase.co',
   SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_imVDY4_4fWx9oECG8-KSWg_Sw8ZtM8S',
-  DOWNLOAD_URL: 'https://lordico.github.io/printly/downloads/Printly_Setup_v0.2.8.4.exe'
+  DOWNLOAD_URL: 'https://lordico.github.io/printly/downloads/Printly_Setup_v0.2.8.4.exe',
+  BUY_URL: 'https://uiesczevcmkzdtybxuez.supabase.co/functions/v1/printly-buy'
 };
