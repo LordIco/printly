3ebade05ee2b6f7abdc8fbea0f7aa2165b201018
LordIco <ico@terra.com.br>
2026-09-04 17:05:38 -0300

    Usa redirecionamento remoto para download do Printly

--- assets/js/download-config.js
+++ assets/js/download-config.js
@@ -1,7 +1,7 @@
 window.PRINTLY_DOWNLOAD_CONFIG = {
   SUPABASE_URL: 'https://uiesczevcmkzdtybxuez.supabase.co',
   SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_imVDY4_4fWx9oECG8-KSWg_Sw8ZtM8S',
-  DOWNLOAD_URL: 'https://lordico.github.io/printly/downloads/Printly_Setup_v0.2.8.4.exe',
+  DOWNLOAD_URL: 'https://uiesczevcmkzdtybxuez.supabase.co/functions/v1/printly-link?key=download_url&source=site',
   BUY_URL: 'https://uiesczevcmkzdtybxuez.supabase.co/functions/v1/printly-buy'
 };
 
